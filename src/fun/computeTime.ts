2071519602a064b21e9f043010883856fc06fba2
skywalker <houzhenhong@hotmail.com>
2019-11-26 20:29:08 +0800

    fix: 有些时候没法获取的问题

--- src/fun/computeTime.ts
+++ src/fun/computeTime.ts
@@ -1,15 +1,19 @@
 import dayjs from 'dayjs';
-
-const nowWeek = sessionStorage.getItem('nowWeek');
+import { useMemo } from 'react';
 
 export default (week: string, weekday: string) => {
+  const nowWeek = sessionStorage.getItem('nowWeek');
   const tillDay = nowWeek
     ? (Number(week) - 1) * 7 +
       Number(weekday) -
       ((Number(nowWeek) - 1) * 7 + dayjs().get('day'))
     : 0;
   const msg = tillDay > 0 ? '还有' : '已过';
-  return `${dayjs()
-    .add(tillDay, 'day')
-    .format('MM-DD')} - ${msg}${Math.abs(tillDay)}天`;
+  return useMemo(
+    () =>
+      `${dayjs()
+        .add(tillDay, 'day')
+        .format('MM-DD')} - ${msg}${Math.abs(tillDay)}天`,
+    [msg, tillDay],
+  );
 };

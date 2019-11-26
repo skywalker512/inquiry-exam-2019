9efd15abcddd7c01aeacccec167e07569efceecc
skywalker <houzhenhong@hotmail.com>
2019-11-26 23:11:17 +0800

    refactor: 简化计算

--- src/fun/computeTime.ts
+++ src/fun/computeTime.ts
@@ -4,9 +4,9 @@ import { useMemo } from 'react';
 export default (week: string, weekday: string) => {
   const nowWeek = sessionStorage.getItem('nowWeek');
   const tillDay = nowWeek
-    ? (Number(week) - 1) * 7 +
+    ? Number(week) * 7 +
       Number(weekday) -
-      ((Number(nowWeek) - 1) * 7 + dayjs().get('day'))
+      (Number(nowWeek) * 7 + dayjs().get('day'))
     : 0;
   const msg = tillDay > 0 ? '还有' : '已过';
   return useMemo(

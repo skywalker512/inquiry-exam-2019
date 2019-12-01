9f3bd2c265f8b0d9c18160a65361c6307cc6690b
skywalker <houzhenhong@hotmail.com>
2019-12-01 22:07:49 +0800

    fix: 星期天为 0 的问题

--- src/fun/computeTime.ts
+++ src/fun/computeTime.ts
@@ -3,10 +3,10 @@ import { useMemo } from 'react';
 
 export default (week: string, weekday: string) => {
   const nowWeek = sessionStorage.getItem('nowWeek');
+  const usDay = dayjs().get('day');
+  const day = usDay || 7;
   const tillDay = nowWeek
-    ? Number(week) * 7 +
-      Number(weekday) -
-      (Number(nowWeek) * 7 + dayjs().get('day'))
+    ? Number(week) * 7 + Number(weekday) - (Number(nowWeek) * 7 + day)
     : 0;
   const msg = tillDay > 0 ? '还有' : '已过';
   return useMemo(
